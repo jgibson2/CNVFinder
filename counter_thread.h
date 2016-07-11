f4214d0c72124f2fe8a59f774a1ec7d1daa3087c
John E. Gibson II <john@erehwon.fourrealllc.com>
2016-07-10 22:48:58 -0400

    Removed unneeded files

--- counter_thread.h
+++
@@ -1,8 +0,0 @@
-
-class CounterThread
-{
-	kmer_sequence_data data;
-	kmer_hash hash;
-	pthread_t thread;
-	pthread_mutex_t mutex;
-	std::function func<void*(void*)>;
--- test.sh
+++
@@ -1,4 +0,0 @@
-for x in {1..20}
-do 
-	time ./test.out $x 
-done
